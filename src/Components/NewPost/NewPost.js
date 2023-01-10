6e85950c3dbe583c4f9236e46ec3ba6fda01ae59
Charles Bartlett <ownageunit@gmail.com>
2023-01-09 20:42:54 -0500

    modal preview and basic inputs done

--- src/Components/NewPost/NewPost.js
+++ src/Components/NewPost/NewPost.js
@@ -1,3 +1,4 @@
+import "./NewPost.css";
 import { useState, useEffect, useRef } from "react";
 import Modal from "../Modal/Modal";
 
@@ -16,12 +17,39 @@ const NewPost = () => {
       document.removeEventListener("mousedown", handler);
     };
   });
+
+  const [file, setFile] = useState(null);
+  const [postName, setPostName] = useState(null);
+
+  const handleFile = (e) => {
+    console.log(e.target.files);
+    setFile(URL.createObjectURL(e.target.files[0]));
+  };
+
+  const handleChange = (e) => {
+    setPostName(e.target.value);
+  };
+
   return (
     <div className="NewPost">
       <button onClick={() => setIsOpen(true)}>Add Your Photo</button>
       <Modal isOpen={isOpen}>
-        <div ref={modalRef}>
-          <h1>Woot Woot</h1>
+        <div ref={modalRef} className="NewPostModal">
+          {/* <form> */}
+          {/* <label>Choose Photo:</label> */}
+          <input type="file" onChange={handleFile} />
+          <h1>IMG below</h1>
+          {file ? (
+            <img src={file} alt={file.name} width="200px" />
+          ) : (
+            <img
+              src="https://i.pinimg.com/564x/eb/bb/b4/ebbbb41de744b5ee43107b25bd27c753.jpg"
+              width="200px"
+            />
+          )}
+          <input type="text" onChange={(e) => handleChange(e)} />
+          <button>Post</button>
+          {/* </form> */}
         </div>
       </Modal>
     </div>
